a936a7a53836a1a6302e446014271d7179e64ddb
Tigran <tigran@manot.ai>
2024-07-05 16:26:41 +0400

    changes

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='my_package',
+    name='connector',
     version='0.1.0',
     packages=find_packages(include=['connector', 'connector.*']),
     install_requires=[
