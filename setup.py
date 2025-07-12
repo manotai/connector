ff7afbcc4df992afe3536d7d9f2be13aac666fdc
Tigran <tigran@manot.ai>
2025-07-12 14:32:16 +0400

    added intent, issue, remove version from report

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='connector',
-    version='0.5.3',
+    version='0.5.4',
     packages=find_packages(include=['connector', 'connector.*']),
     install_requires=[
         'elasticsearch~=8.13.0',
